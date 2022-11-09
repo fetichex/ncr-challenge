73c5f60221575a258c73d6afb157b06413cb7bd1
diego <dfcmera@gmail.com>
2022-11-09 13:26:38 -0300

    [feat]:create app component - test - setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import { useEffect } from 'react'
+import { Route } from 'wouter'
+
+import useAccountStore from './store'
+import { ROUTES } from './utils'
+import Layout from './components/Layout'
+import Global from './global.style'
+
 export default function App() {
-  return <></>
+  const getAcc = useAccountStore((state) => state.getAcc)
+  useEffect(() => {
+    getAcc('http://localhost:3000/')
+  }, [])
+
+  return (
+    <>
+      <Global />
+      <section className='App'>
+        <Layout id='layout'>
+          {ROUTES.map(({ path, component }) => (
+            <Route key={path} path={path} component={component}></Route>
+          ))}
+        </Layout>
+      </section>
+    </>
+  )
 }
