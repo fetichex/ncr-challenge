b1b5bfd3cce0b6132a07a160bf605f325b0bd8e4
diego <dfcmera@gmail.com>
2022-11-09 16:10:29 -0300

    [feat]:create account info component

--- src/components/Account/Account.test.jsx
+++ src/components/Account/Account.test.jsx
@@ -1,11 +1,32 @@
-import { render } from '../../utils/test-utils'
+import { render, screen } from '../../utils/test-utils'
 import Account from './Account'
 
 describe('Account', () => {
-  it('should render the component', () => {
-    render(<Account />)
-  })
-  it('should render the component', ({ t = 'cc', n = 1, i = 1 }) => {
+  it('should render the component', ({ t = 'ca', n = 599238, i = 3 }) => {
     render(<Account t={t} n={n} i={i} />)
+    screen.debug()
+  })
+  it('"t" should be a string, "n" and "i" should be numbers', () => {
+    const t = 'cc'
+    const n = 8994738
+    const i = 2
+    render(<Account t={t} n={n} i={i} />)
+    expect(typeof t).toBe('string')
+    expect(typeof n).toBe('number')
+    expect(typeof i).toBe('number')
+  })
+  it('should render "Cuenta Corriente" if "t" is equal to "CC"', () => {
+    const t = 'cc'
+    const n = 8994738
+    const i = 2
+    render(<Account t={t} n={n} i={i} />)
+    expect(screen.getByText(/cuenta corriente/i)).toBeInTheDocument()
+  })
+  it('should render "Caja de Ahorros" if "t" is equal to "CA"', () => {
+    const t = 'ca'
+    const n = 8994738
+    const i = 2
+    render(<Account t={t} n={n} i={i} />)
+    expect(screen.getByText(/caja de ahorros/i)).toBeInTheDocument()
   })
 })
