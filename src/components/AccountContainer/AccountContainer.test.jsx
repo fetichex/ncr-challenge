2a79d5ed44af3694b3083266cc5b97cc4d75c99c
diego <dfcmera@gmail.com>
2022-11-18 18:25:45 -0300

    feat(refactor): refactor logic

--- src/components/AccountContainer/AccountContainer.test.jsx
+++ src/components/AccountContainer/AccountContainer.test.jsx
@@ -2,19 +2,8 @@ import { customRender, screen } from '../../utils/test-utils'
 import AccountContainer from './AccountContainer'
 
 describe('AccountContainer', () => {
-  it('should customRender de component', () => {
+  it('should render de component', () => {
     customRender(<AccountContainer />)
     screen.debug()
-  })
-  it('should customRender text "Selecciona la Cuenta a Consultar"', () => {
-    customRender(<AccountContainer />)
-    const text = screen.getByText(/Selecciona la Cuenta a Consultar/i)
-    expect(text).toBeInTheDocument()
-  })
-  it('"Selecciona la Cuenta a Consultar" should be a h1', () => {
-    customRender(<AccountContainer />)
-    const text = screen.getByText(/Selecciona la Cuenta a Consultar/i)
-    const h1 = document.querySelector('h1')
-    expect(text === h1).toBe(true)
   })
 })
